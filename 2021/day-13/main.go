7727bd3d6ad9204fdea6e9c678a8a65f9b4f8b8e
Lawrence Brooks <84.lbrooks@gmail.com>
2021-12-14 00:01:38 -0500

    Update part functions to relfect question

--- 2021/day-13/main.go
+++ 2021/day-13/main.go
@@ -81,7 +81,6 @@ func playOne(input []string) {
 
 			points[coord{col: col, row: row}] = struct{}{}
 		} else if strings.Contains(v, "=") {
-			fmt.Printf("Number of points remaining: %d\n", len(points))
 			col, row := -1, -1
 			foldOn, _ := strconv.Atoi(v[13:])
 			switch v[11] {
@@ -91,7 +90,37 @@ func playOne(input []string) {
 				row = foldOn
 			}
 
-			// printBoard(points, col, row)
+			for c := range points {
+				delete(points, c)
+				points[c.fold(col, row)] = struct{}{}
+			}
+
+			break
+		}
+	}
+
+	fmt.Printf("Number of points remaining: %d\n", len(points))
+}
+
+func playTwo(input []string) {
+	points := make(map[coord]struct{})
+
+	for _, v := range input {
+		if strings.Contains(v, ",") {
+			c := strings.Split(v, ",")
+			col, _ := strconv.Atoi(c[0])
+			row, _ := strconv.Atoi(c[1])
+
+			points[coord{col: col, row: row}] = struct{}{}
+		} else if strings.Contains(v, "=") {
+			col, row := -1, -1
+			foldOn, _ := strconv.Atoi(v[13:])
+			switch v[11] {
+			case 'x':
+				col = foldOn
+			case 'y':
+				row = foldOn
+			}
 
 			for c := range points {
 				delete(points, c)
@@ -101,11 +130,6 @@ func playOne(input []string) {
 	}
 
 	printBoard(points, -1, -1)
-	fmt.Printf("Number of points remaining: %d\n", len(points))
-}
-
-func playTwo(input []string) {
-	fmt.Println("Not Yet Implemented")
 }
 
 func main() {
