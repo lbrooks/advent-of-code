066b3e34b4cde02118c7a0553e77e16dc1a1d77b
Lawrence Brooks <84.lbrooks@gmail.com>
2022-12-03 13:51:46 -0500

    restructuring

--- run_cc.sh
+++ run_cc.sh
@@ -1,3 +1,3 @@
 #!/opt/homebrew/bin/fish
 
-bazel run //$argv[1]/cc:day$argv[2] -- (pwd)/$argv[1]/day$argv[2].txt
+bazel run //$argv[1]/cc:day$argv[2] -- (pwd)/$argv[1]/input/day$argv[2].txt
--- run_go.sh
+++ run_go.sh
@@ -1,3 +1,3 @@
 #!/opt/homebrew/bin/fish
 
-cat $argv[1]/day$argv[2].txt | bazel run //$argv[1]/go:day$argv[2]
+cat $argv[1]/input/day$argv[2].txt | bazel run //$argv[1]/go:day$argv[2]
